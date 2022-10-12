ce2de02446d54393259af1b7f9559474e5424b83
Mark De Souza <markcdesouza@gmail.com>
2022-10-13 10:13:12 +1100

    - Added window listeners to capture saving window position to   preferences - Removed services like video calling that rely on Atlassian and no longer work - Added css styling to fix rendering issues

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ A modern HipChat client to work with the abandoned HipChat server from Atlassian
 - Fixes the right click crash from the official Atlassian client (under MacOS 12.0+)  **(done)**
 - Fixes the non-responsive search bar (under MacOS 12.0+)  **(done)**
 - Fixes the intermittent logout issue  **(done)**
-- Removes services like video calling that rely on Atlassian and no longer work
+- Removes services like video calling that rely on Atlassian and no longer work **(done)**
 - Opens links in your default browser
 - Minimizes to a tray icon & adds tray based notifications
 - Desktop notifications
@@ -32,3 +32,11 @@ A modern HipChat client to work with the abandoned HipChat server from Atlassian
         $ yarn build-win
         or
         $ yarn build-linux
+
+## Arguments
+
+    Ignore server url saved in preferences
+    $ yarn start --new-chat
+
+    Open new chat:
+    $ yarn start --new-chat
--- app/main.js
+++ app/main.js
@@ -1,20 +1,19 @@
-'use strict';
+'use strict'
 
-const electron = require('electron');
-const open = require('open');
-const path = require('path');
-const storage = require('electron-json-storage');
-const prompt = require('electron-prompt');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const ipcMain = electron.ipcMain;
-const nativeImage = electron.nativeImage;
+const electron = require('electron')
+const open = require('open')
+const path = require('path')
+const storage = require('electron-json-storage')
+const prompt = require('electron-prompt')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const ipcMain = electron.ipcMain
+const nativeImage = electron.nativeImage
 
-const DEFAULT_SERVER_URL = "";
-
-let mainWindow = null;
-let appIcon = null;
-let prefs = {};
+const DEFAULT_SERVER_URL = ''
+let mainWindow = null
+let appIcon = null
+let prefs = {}
 
 // Handle icon initialisation
 const iconPaths = {
@@ -38,18 +37,19 @@ const iconPaths = {
         normal: 'images/hipchat-dock.png',
         alert: 'images/hipchat-dock.png',
     },
-};
+}
 
-let normalTrayIconPath = path.resolve(path.join(__dirname, (process.platform in iconPaths) ? iconPaths[process.platform].normal : iconPaths.default.normal));
-let normalTrayIconImage = nativeImage.createFromPath(normalTrayIconPath);
-let alertTrayIconPath = path.resolve(path.join(__dirname, (process.platform in iconPaths) ? iconPaths[process.platform].alert : iconPaths.default.alert));
-let alertTrayIconImage = nativeImage.createFromPath(alertTrayIconPath);
-let dockIconPath = path.resolve(path.join(__dirname, iconPaths.dock.normal));
-let dockIconImage = nativeImage.createFromPath(dockIconPath);
+let normalTrayIconPath = path.resolve(path.join(__dirname, (process.platform in iconPaths) ? iconPaths[process.platform].normal : iconPaths.default.normal))
+let normalTrayIconImage = nativeImage.createFromPath(normalTrayIconPath)
+let alertTrayIconPath = path.resolve(path.join(__dirname, (process.platform in iconPaths) ? iconPaths[process.platform].alert : iconPaths.default.alert))
+let alertTrayIconImage = nativeImage.createFromPath(alertTrayIconPath)
+let dockIconPath = path.resolve(path.join(__dirname, iconPaths.dock.normal))
+let dockIconImage = nativeImage.createFromPath(dockIconPath)
 
 if (process.platform == 'darwin') {
     app.dock.setIcon(dockIconImage)
 }
+
 
 function loadChatWindow() {
     var electronScreen = electron.screen
@@ -58,32 +58,71 @@ function loadChatWindow() {
     var defaultHeight = ('windowHeight' in prefs) ? prefs['windowHeight'] : Math.floor(primaryDisplay.workAreaSize.height * 0.9)
     var defaultZoom = ('zoom' in prefs) ? prefs['zoom'] : 1.0
 
-    // Prepare window options
+    // Open Chat Window
     var windowOptions = {
-      title: 'HipChat',
-      titleBarStyle: 'hiddenInset',
-      spellcheck: true,
-      width: defaultWidth,
-      height: defaultHeight,
-      icon: path.resolve(path.join(__dirname, 'images/256x256/hipchat.png')),
-      webPreferences: {
-        zoomFactor: defaultZoom,
-        nodeIntegration: false,
-        preload: path.resolve(path.join(__dirname, 'preload.js')),
-        contextIsolation: false,
-        allowDisplayingInsecureContent: true,
-      }
+        title: 'HipChat',
+        titleBarStyle: 'hiddenInset',
+        spellcheck: true,
+        width: defaultWidth,
+        height: defaultHeight,
+        icon: path.resolve(path.join(__dirname, 'images/256x256/hipchat.png')),
+        webPreferences: {
+            zoomFactor: defaultZoom,
+            nodeIntegration: false,
+            preload: path.resolve(path.join(__dirname, 'preload.js')),
+            contextIsolation: false,
+            allowDisplayingInsecureContent: true,
+        }
     }
-    if ('windowX' in prefs) 
+    if ('windowX' in prefs)
         windowOptions['x'] = prefs['windowX']
-    if ('windowY' in prefs) 
+    if ('windowY' in prefs)
         windowOptions['y'] = prefs['windowX']
     mainWindow = new BrowserWindow(windowOptions)
-    mainWindow.loadURL(prefs.server_url);
+    mainWindow.loadURL(prefs.server_url)
+
+    // Window Event Listeners
+    mainWindow.on('close', function (event) {
+        quit()
+    })
+
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+
+    mainWindow.on('resize', function (event) {
+        var size = mainWindow.getSize()
+        prefs['windowWidth'] = size[0]
+        prefs['windowHeight'] = size[1]
+        savePrefs(false)
+    });
+
+    mainWindow.on('move', function (event) {
+        var position = mainWindow.getPosition()
+        prefs['windowX'] = position[0]
+        prefs['windowY'] = position[1]
+        savePrefs(false)
+    })
+
+    let contents = mainWindow.webContents
+    contents.on('did-finish-load', () => {
+        contents.insertCSS('#logo { margin-left: 60px; } header { -webkit-user-select: none;  -webkit-app-region: drag; } .activity-icon{ display:none; } .hc-video-call-btn-link { display:none !important; } ')
+    })
 }
 
 function quit() {
-    app.quit();
+    app.quit()
+}
+
+function savePrefs(exitOnFail) {
+    storage.set('prefs', prefs, function (error) {
+        if (error) {
+            console.log("Couldn't save preferences due to storage error: ", error)
+            if (exitOnFail) {
+                quit()
+            }
+        }
+    })
 }
 
 app.on('ready', function () {
@@ -96,19 +135,20 @@ app.on('ready', function () {
             showAndFocusWindow()
 
             if (argv.includes('--new-chat')) {
-                newChat();
+                newChat()
             }
         })
     }
 
     storage.get('prefs', function (error, data) {
         if (error) {
-            console.log("Could not load preferences from storage:", error, data);
+            console.log("Could not load preferences from storage:", error, data)
         }
-        prefs = data;
+        prefs = data
 
         if (!prefs || !prefs.server_url || process.argv.includes('--logout')) {
             prefs.server_url = DEFAULT_SERVER_URL
+            savePrefs(true)
         }
 
         if (prefs.server_url === "") {
@@ -123,12 +163,7 @@ app.on('ready', function () {
             }).then((input) => {
                 if (input !== null && input.trim() !== "") {
                     prefs.server_url = input
-                    storage.set('prefs', prefs, function (error) {
-                        if (error) {
-                            console.log("Couldn't save preferences due to storage error: ", error)
-                            quit()
-                        }
-                    })
+                    savePrefs(true)
                     loadChatWindow()
                 } else {
                     console.log("HipChat Server URL can not be empty")
