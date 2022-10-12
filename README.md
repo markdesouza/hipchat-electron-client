647a23f609a956d00c0ff52b96cd88224687f31e
Mark De Souza <markcdesouza@gmail.com>
2022-10-13 09:54:14 +1100

    Initial prototype: - Sets dock icon (macos) - Loads preferences (including server url) - Asks for server url if isn't set - Opens a browser window to the correct url

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # hipchat-electron-client
 A modern HipChat client to work with the abandoned HipChat server from Atlassian
+
+## Features
+
+- Produces builds for Mac (Universal / M1 compatible), Windows, Linux  **(done)**
+- Fixes the right click crash from the official Atlassian client (under MacOS 12.0+)  **(done)**
+- Fixes the non-responsive search bar (under MacOS 12.0+)  **(done)**
+- Fixes the intermittent logout issue  **(done)**
+- Removes services like video calling that rely on Atlassian and no longer work
+- Opens links in your default browser
+- Minimizes to a tray icon & adds tray based notifications
+- Desktop notifications
+- Enhanced keyboard shortcuts
+
+## Build and run from source
+
+1. Checkout this repository and install dependencies
+
+        $ git checkout https://github.com/markdesouza/hipchat-electron-client
+        $ cd hipchat-electron-client
+        $ yarn install
+
+2. Start the application
+
+        $ yarn start
+
+## Produce builds
+
+        $ yarn build-mac
+        or
+        $ yarn build-win
+        or
+        $ yarn build-linux
---
+++ app/main.js
@@ -0,0 +1,142 @@
+'use strict';
+
+const electron = require('electron');
+const open = require('open');
+const path = require('path');
+const storage = require('electron-json-storage');
+const prompt = require('electron-prompt');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const ipcMain = electron.ipcMain;
+const nativeImage = electron.nativeImage;
+
+const DEFAULT_SERVER_URL = "";
+
+let mainWindow = null;
+let appIcon = null;
+let prefs = {};
+
+// Handle icon initialisation
+const iconPaths = {
+    linux: {
+        normal: 'images/64x64/hipchat-mono.png',
+        alert: 'images/64x64/hipchat-mono-alert.png',
+    },
+    win32: {
+        normal: 'images/16x16/hipchat.png',
+        alert: 'images/16x16/hipchat-alert.png',
+    },
+    darwin: {
+        normal: 'images/32x32/hipchat-mono@2x.png',
+        alert: 'images/32x32/hipchat-attention@2x.png',
+    },
+    default: {
+        normal: 'images/hipchat-default.png',
+        alert: 'images/hipchat-default.png',
+    },
+    dock: {
+        normal: 'images/hipchat-dock.png',
+        alert: 'images/hipchat-dock.png',
+    },
+};
+
+let normalTrayIconPath = path.resolve(path.join(__dirname, (process.platform in iconPaths) ? iconPaths[process.platform].normal : iconPaths.default.normal));
+let normalTrayIconImage = nativeImage.createFromPath(normalTrayIconPath);
+let alertTrayIconPath = path.resolve(path.join(__dirname, (process.platform in iconPaths) ? iconPaths[process.platform].alert : iconPaths.default.alert));
+let alertTrayIconImage = nativeImage.createFromPath(alertTrayIconPath);
+let dockIconPath = path.resolve(path.join(__dirname, iconPaths.dock.normal));
+let dockIconImage = nativeImage.createFromPath(dockIconPath);
+
+if (process.platform == 'darwin') {
+    app.dock.setIcon(dockIconImage)
+}
+
+function loadChatWindow() {
+    var electronScreen = electron.screen
+    var primaryDisplay = electronScreen.getPrimaryDisplay()
+    var defaultWidth = ('windowWidth' in prefs) ? prefs['windowWidth'] : Math.floor(primaryDisplay.workAreaSize.width * 0.9)
+    var defaultHeight = ('windowHeight' in prefs) ? prefs['windowHeight'] : Math.floor(primaryDisplay.workAreaSize.height * 0.9)
+    var defaultZoom = ('zoom' in prefs) ? prefs['zoom'] : 1.0
+
+    // Prepare window options
+    var windowOptions = {
+      title: 'HipChat',
+      titleBarStyle: 'hiddenInset',
+      spellcheck: true,
+      width: defaultWidth,
+      height: defaultHeight,
+      icon: path.resolve(path.join(__dirname, 'images/256x256/hipchat.png')),
+      webPreferences: {
+        zoomFactor: defaultZoom,
+        nodeIntegration: false,
+        preload: path.resolve(path.join(__dirname, 'preload.js')),
+        contextIsolation: false,
+        allowDisplayingInsecureContent: true,
+      }
+    }
+    if ('windowX' in prefs) 
+        windowOptions['x'] = prefs['windowX']
+    if ('windowY' in prefs) 
+        windowOptions['y'] = prefs['windowX']
+    mainWindow = new BrowserWindow(windowOptions)
+    mainWindow.loadURL(prefs.server_url);
+}
+
+function quit() {
+    app.quit();
+}
+
+app.on('ready', function () {
+    let instanceLock = app.requestSingleInstanceLock()
+    if (!instanceLock) {
+        console.log("An instance is already running. Exiting...")
+        quit()
+    } else {
+        app.on('second-instance', (event, argv, cwd) => {
+            showAndFocusWindow()
+
+            if (argv.includes('--new-chat')) {
+                newChat();
+            }
+        })
+    }
+
+    storage.get('prefs', function (error, data) {
+        if (error) {
+            console.log("Could not load preferences from storage:", error, data);
+        }
+        prefs = data;
+
+        if (!prefs || !prefs.server_url || process.argv.includes('--logout')) {
+            prefs.server_url = DEFAULT_SERVER_URL
+        }
+
+        if (prefs.server_url === "") {
+            prompt({
+                title: 'Enter HipChat Server Address',
+                label: 'URL:',
+                value: 'https://hipchat.workplace.com',
+                inputAttrs: {
+                    type: 'url'
+                },
+                type: 'input'
+            }).then((input) => {
+                if (input !== null && input.trim() !== "") {
+                    prefs.server_url = input
+                    storage.set('prefs', prefs, function (error) {
+                        if (error) {
+                            console.log("Couldn't save preferences due to storage error: ", error)
+                            quit()
+                        }
+                    })
+                    loadChatWindow()
+                } else {
+                    console.log("HipChat Server URL can not be empty")
+                    quit()
+                }
+            }).catch(console.error)
+        } else {
+            loadChatWindow()
+        }
+    })
+})
